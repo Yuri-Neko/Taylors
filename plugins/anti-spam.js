04cbd7f7ef5c5cd49af2b2db9ab5325c758c4433
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-08-03 10:57:13 +0800

    Add files via upload

--- plugins/anti-spam.js
+++ plugins/anti-spam.js
@@ -1,43 +1,35 @@
 import { performance } from 'perf_hooks';
 
 export async function before(m) {
-  this.spamData = this.spamData || {};
-  const chatData = global.db.data.chats[m.chat];
-  const senderId = m.sender;
-  const userData = global.db.data.users[senderId];
+    const users = global.db.data.users;
+    const chats = global.db.data.chats;
 
-  this.spamData[senderId] = this.spamData[senderId] || { count: 0, lastMsgTime: 0, isSpam: false, cooldownEndTime: 0 };
-  const now = performance.now();
-  const timeDifference = now - this.spamData[senderId].lastMsgTime;
+    if (!chats[m.chat].antiSpam) return;
+    if (m.isBaileys) return;
+    if (!m.message) return;
+    if (users[m.sender].banned || chats[m.chat].isBanned) return;
 
-  if (chatData.antiSpam && !m.isBaileys) {
-    if (this.spamData[senderId].isSpam && now < this.spamData[senderId].cooldownEndTime) {
-      const remainingCooldown = Math.ceil((this.spamData[senderId].cooldownEndTime - now) / 1000);
-      if (m.isCommand) {
-        const waitCooldownMessage = `⏳ *Cooldown*\nTunggu setelah ${remainingCooldown} detik`; // Message displayed when the user is asked to wait during the cooldown period.
-        await this.reply(m.chat, waitCooldownMessage, m, { mentions: [senderId] });
-      }
-      return;
+    this.spam = this.spam || {};
+    this.spam[m.sender] = this.spam[m.sender] || { count: 0, lastspam: 0 };
+    const now = performance.now();
+    const timeDifference = now - this.spam[m.sender].lastspam;
+
+    if (timeDifference < 10000) {
+        this.spam[m.sender].count++;
+        if (this.spam[m.sender].count >= 5) {
+            users[m.sender].banned = true;
+            this.spam[m.sender].lastspam = now + 5000;
+            const remainingCooldown = Math.ceil((this.spam[m.sender].lastspam - now) / 1000);
+            setTimeout(() => {
+                users[m.sender].banned = false;
+                this.spam[m.sender].count = 0;
+                m.reply(`✅ *Cooldown selesai*\nAnda bisa mengirim pesan lagi.`);
+            }, 5000);
+            return m.reply(`❌ *Mohon jangan spam*\nTunggu setelah ${remainingCooldown} detik`);
+        }
+    } else {
+        this.spam[m.sender].count = 0;
     }
 
-    this.spamData[senderId].count++;
-    this.spamData[senderId].lastMsgTime = now;
-
-    if (this.spamData[senderId].count >= 5 && timeDifference < 5000) {
-      userData.banned = true;
-      this.spamData[senderId].isSpam = true;
-      this.spamData[senderId].cooldownEndTime = now + 5000;
-      const remainingCooldown = Math.ceil((this.spamData[senderId].cooldownEndTime - now) / 1000);
-
-      setTimeout(() => {
-        userData.banned = false;
-        this.spamData[senderId].isSpam = false;
-        m.reply(`✅ *Cooldown selesai*\nAnda bisa mengirim pesan lagi.`);
-      }, 5000);
-
-      this.spamData[senderId].count = 0;
-      const spamWarningMessage = `❌ *Mohon jangan spam*\nTunggu setelah ${remainingCooldown} detik`; // Message displayed when the user has been spamming and is asked to wait.
-      await this.reply(m.chat, spamWarningMessage, m, { mentions: [senderId] });
-    }
-  }
+    this.spam[m.sender].lastspam = now;
 }
